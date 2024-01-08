638ab8e2ca77b46e554ec517af7d64d918203ce0
Bruna Fernandes <bruna@evnts.com.br>
2024-01-08 19:40:25 -0300

    trocando de maquina

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,1 +1,7 @@
 import {test, expect} from '@playwright/test'
+
+test('Webapp deve estar online', async ({page}) => {
+    await page.goto('http://172.26.96.1:8080')
+    await expect(page).toHaveTitle('Gerencie suas tarefas com Mark L')
+    await page.waitForTimeout(3000)
+})
